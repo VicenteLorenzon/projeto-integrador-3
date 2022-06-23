5e78423426a7f9fc91e23588c62fcd38ec7d7402
Vicente Lorenzon <vicente.lorenzon@systempro.com.br>
2022-06-23 11:58:34 -0300

    Alterações em tudo

--- main/urls.py
+++ main/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('carrinho/', views.carrinho, name='carrinho'),
     path('mandar_carrinho/<int:produto>', views.mandar_carrinho, name='mandar_carrinho'),
     path('tirar_carrinho/<int:item>', views.tirar_carrinho, name='tirar_carrinho'),
-    path('confirmar_servico/', views.confirmar_servico, name='confirmar_servico'),
     path('produto/<int:id>', views.produto, name='produto'),
     path('editar_pet/<int:id>', views.editar_pet, name='editar_pet'),
     path('finalizar_compra/', views.finalizar_compra, name='finalizar_compra'),
--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from imp import reload
 from threading import excepthook
 from urllib.error import URLError
 from wsgiref.util import request_uri
+from xmlrpc.client import DateTime
 from django.shortcuts import redirect, render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse, HttpRequest, HttpResponseForbidden, HttpResponseRedirect
 from .validations import campos_em_branco, senhas_iguais
@@ -10,34 +11,14 @@ from django.contrib.auth.models import User
 from .models import *
 from django.contrib import auth
 from django.views.generic.base import RedirectView
+from datetime import date
 
 def index(request):
     produtos = Produto.objects.all()
     return render(request, 'index.html', {'produtos': produtos})
 
-def adicionar_pet(request):
-    return render(request, 'adicionar_pet.html')
-
-def login(request):
-    if request.method == 'POST':
-        mensagens_erro = []
-        email = request.POST['email']
-        senha = request.POST['senha']
-        user = auth.authenticate(request, username=email, password=senha)
-
-        campos_em_branco([email, senha], mensagens_erro)
-
-        if user is not None:
-            auth.login(request, user)
-            if request.user.is_staff:
-                return HttpResponseRedirect('/admin/')
-            else:
-                return redirect('meus_dados')
-        else:
-            mensagens_erro.append('E-mail e senha não combinam')
-        return render(request, 'login.html', {'erros': mensagens_erro})
-    else:
-        return render(request, 'login.html')
+def perguntas_frequentes(request):
+    return render(request, 'perguntas_frequentes.html')
 
 def cadastro(request):
     if request.method == 'POST':
@@ -63,6 +44,72 @@ def cadastro(request):
     else:
         return render(request, 'cadastro.html')
 
+def login(request):
+    if request.method == 'POST':
+        mensagens_erro = []
+        email = request.POST['email']
+        senha = request.POST['senha']
+        user = auth.authenticate(request, username=email, password=senha)
+
+        campos_em_branco([email, senha], mensagens_erro)
+
+        if user is not None:
+            auth.login(request, user)
+            if request.user.is_staff:
+                return HttpResponseRedirect('/admin/')
+            else:
+                return redirect('meus_dados')
+        else:
+            mensagens_erro.append('E-mail e senha não combinam')
+        return render(request, 'login.html', {'erros': mensagens_erro})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
+
+def meus_dados(request): #FALTA TUDO
+    return render(request, 'meus_dados.html')
+
+def meus_pets(request):
+    pets = Animal.objects.filter(user=request.user.id)
+    return render(request, 'meus_pets.html', {'pets': pets})
+
+def adicionar_pet(request): #FALTA AJUSTAR TEMPLATE
+    if request.method == 'get':
+        return render(request, 'adicionar_pet.html')  
+
+    elif request.method == 'post':
+        pet = Animal(
+            nome = request.POST['nome'].strip(),
+            user = request.user,
+            especie = Especie.objects.get(id=request.POST['especie']),
+            raca = Raca.objects.get(id=request.POST['raca']),
+            cor = request.POST['cor'].strip(),
+            aniversario = request.POST['aniversario'],
+            foto = request.POST['foto']
+        )
+        pet.save()
+
+def editar_pet(request, id): #FALTA AJUSTAR TEMPLATE
+    pet = Animal.objects.get(id=id)
+    if request.user != pet.user:
+        return HttpResponseForbidden
+
+    elif request.method == 'GET':
+        return render(request, 'editar_pet.html', {'pet': pet})
+
+    elif request.method == 'POST':
+        pet.nome = request.POST['nome'].strip()
+        pet.especie = Especie.objects.get(id=request.POST['especie'])
+        pet.raca = Raca.objects.get(id=request.POST['raca'])
+        pet.cor = request.POST['cor'].strip()
+        pet.aniversario = request.POST['aniversario']
+        pet.foto = request.POST['foto']
+        pet.save()
+        return redirect('meus_pets')
+
 def carrinho(request):
     itens = Carrinho.objects.filter(user=request.user.id)
     return render(request, 'carrinho.html', context={'carrinho': itens})
@@ -73,46 +120,44 @@ def mandar_carrinho(request, produto):
     return redirect('carrinho')
 
 def tirar_carrinho(request, item):
-    i = Carrinho.objects.get(id=item).delete()
+    item_carrinho = Carrinho.objects.get(id=item)
+    if item_carrinho.user != request.user:
+        return HttpResponseForbidden
+
+    item_carrinho.delete()
     return redirect('carrinho')
 
-def confirmar_servico(request):
-    return render(request, 'confirmar_servico.html')
+def finalizar_compra(request):
+    carrinho_user = Carrinho.objects.filter(user=request.user.id)
+    if request.method == 'get':
+        enderecos = Endereco.objects.filter(user=request.user.id)
+        preco = 0
+        for item in carrinho_user:
+            valor_item = item.quantidade * item.valor_unit
+            preco += valor_item
+        return render(request, 'finalizar_compra.html', context={'contexto':{'enderecos': enderecos, 'preco': preco}})
+
+    elif request.method == 'post':
+        forma_entrega = request.POST['forma_entrega']
+        endereco = request.POST['endereco']
+        for item in carrinho_user:
+            venda = Venda(
+                user=request.user,
+                produto=item.produto,
+                data=date.today(),
+                endereco=Endereco.objects.get(id=endereco),
+                forma_entrega=forma_entrega,
+                quantidade=item.quantidade,
+                valor_total=(item.quantidade * item.valor_unit)
+            )
+            venda.save()
+
+        carrinho_user.delete()
+        return redirect('index')
 
 def produto(request, id):
     prod = Produto.objects.get(id=id)
     return render(request, 'dados_produto.html', {'produto': prod})
-
-def editar_pet(request, id):
-    if request.method == 'GET':
-        pet = Animal.objects.get(id=id)
-        return render(request, 'editar_pet.html', {'pet': pet})
-    elif request.method == 'POST':
-        pet = Animal.objects.get(id=id)
-        pet.nome = request.POST['nome']
-        pet.especie = Especie.objects.get(id=request.POST['especie'])
-        pet.raca = Raca.objects.get(id=request.POST['raca'])
-        pet.cor = request.POST['cor']
-        pet.aniversario = request.POST['aniversario']
-        pet.save()
-        return redirect('meus_pets')
-
-def finalizar_compra(request):
-    return render(request, 'finalizar_compra.html')
-
-def logout(request):
-    auth.logout(request)
-    return redirect('index')
-
-def meus_dados(request):
-    return render(request, 'meus_dados.html')
-
-def meus_pets(request):
-    pets = Animal.objects.filter(user=request.user.id)
-    return render(request, 'meus_pets.html', {'pets': pets})
-
-def perguntas_frequentes(request):
-    return render(request, 'perguntas_frequentes.html')
 
 def produtos(request):
     try:
@@ -122,7 +167,7 @@ def produtos(request):
 
     return render(request, 'produtos.html', {'produtos': produtos})
 
-def servicos(request):
+def servicos(request): #FALTA TERMINAR
     pets = Animal.objects.filter(user=request.user.id)   
     try:
         pet = request.GET['pet']
@@ -132,4 +177,4 @@ def servicos(request):
         servicos = Servico_Animal_Disponibilidade.objects.filter(raca=raca, disponivel=True)
         return render(request, 'servicos.html', {'contexto':{'pets': pets, 'servicos': servicos, 'nome_pet': 'Serviços disponíves - '+ Animal.objects.get(id=pet).nome}})
     except:
-        return render(request, 'servicos.html', {'contexto':{'pets': pets}}) 
+        return render(request, 'servicos.html', {'contexto':{'pets': pets}})
