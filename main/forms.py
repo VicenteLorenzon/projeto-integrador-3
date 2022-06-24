55579d4eacafe4cf85fbd442ec0be11a1146639d
Vicente Lorenzon <vicente.lorenzon@systempro.com.br>
2022-06-24 10:45:53 -0300

    Teste regex cpf

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,8 @@
 from pyexpat import model
 from django import forms
-from .models import Animal
+from .models import *
+from django.core.validators import RegexValidator
+
 
 class Adicionar_pet(forms.ModelForm):
     class Meta:
@@ -11,4 +13,12 @@ class Adicionar_pet(forms.ModelForm):
         }
         widgets = {
             'aniversario': forms.DateInput(attrs={'data-mask': '00/00/0000'})
+        }
+    
+class Dados(forms.ModelForm):
+    class Meta:
+        model = User_Dados
+        fields = '__all__'
+        widgets = {
+            'cpf': forms.CharField(validators=[RegexValidator(regex=r'[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}')])
         }
